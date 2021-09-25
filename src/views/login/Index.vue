e160a55b5bc0752300a0a12d4848d83e52736e74
sky0604liang <1065173649@qq.com>
2021-09-25 18:01:25 +0800

    登录页

---
+++ src/views/login/Index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login-box">
+        登录页
+    </div>
+</template>
+<script>
+export default {
+    name:"login"
+}
+</script>
+<style lang="scss" scoped>
+// scoped 针对当前组件有效
+.login-box{
+    height: 100vh;
+    background-color: #344a5f;
+}
+
+</style>>
